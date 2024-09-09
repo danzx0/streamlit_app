3987f7d1ee65e1cff33c6c91ed50669dfaec2800
danzx0 <wadashin17@icloud.com>
2024-09-09 13:31:37 +0900

    import fix 34

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -389,13 +389,14 @@ def graph_drawing_remove_arcs(input_list, z_star, next_z_star, tuple_Arcs, tuple
 
 def display():
     st.header('Input Graph Data')
-    input_list = []
+    if 'input_list' not in st.session_state:
+        st.session_state['input_list'] = []
     # Warningの非表示
     # st.set_option('deprecation.showPyplotGlobalUse', False)
     # 入力の受け取り
     if st.checkbox('input by text'):
         input_text = st.text_input('Graph Data')
-        input_list = list(map(int, input_text.split()))
+        st.session_state['input_list'] = list(map(int, input_text.split()))
     else:
         num_of_nodes = st.slider('number of nodes', 2, 20, 5) # min, max, default
         num_of_arcs = st.slider('number of arcs', num_of_nodes-1, num_of_nodes*(num_of_nodes-1) - (num_of_nodes-1), num_of_nodes-1) # min, max, default
@@ -409,7 +410,7 @@ def display():
                 if i != len(input_list) - 1:
                     input_list_str += ' '
             input_text = st.text_input('Graph Data', value=input_list_str)
-            input_list = list(map(int, input_text.split()))
+            st.session_state['input_list'] = list(map(int, input_text.split()))
 
     # モデル選択
     option_type = st.selectbox('Select the type of Interdiction', ['Cost increase', 'Remove arcs'])
@@ -419,17 +420,17 @@ def display():
     if st.button('Run DSPI'):
         if option_type == 'Cost increase':
             if option_constraints == 'free (Interdict any number of arcs at a time)':
-                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_free.get_DSPI_free(input_list)
-                graph_drawing_cost_increase(input_list, z_star, next_z_star, tuple_Arcs, tuple_A)
+                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_free.get_DSPI_free(st.session_state['input_list'])
+                graph_drawing_cost_increase(st.session_state['input_list'], z_star, next_z_star, tuple_Arcs, tuple_A)
             elif option_constraints == 'at most (Interdict up to one at a time)':
-                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_at_most.get_DSPI_at_most(input_list)
-                graph_drawing_cost_increase(input_list, z_star, next_z_star, tuple_Arcs, tuple_A)
+                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_at_most.get_DSPI_at_most(st.session_state['input_list'])
+                graph_drawing_cost_increase(st.session_state['input_list'], z_star, next_z_star, tuple_Arcs, tuple_A)
             elif option_constraints == 'at least (More than 1 Interdict at a time)':
-                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_at_least.get_DSPI_at_least(input_list)
-                graph_drawing_cost_increase(input_list, z_star, next_z_star, tuple_Arcs, tuple_A)
+                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_at_least.get_DSPI_at_least(st.session_state['input_list'])
+                graph_drawing_cost_increase(st.session_state['input_list'], z_star, next_z_star, tuple_Arcs, tuple_A)
             else:
-                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_exactly.get_DSPI_exactly(input_list)
-                graph_drawing_cost_increase(input_list, z_star, next_z_star, tuple_Arcs, tuple_A)
+                z_star, next_z_star, tuple_Arcs, tuple_A = DSPI_exactly.get_DSPI_exactly(st.session_state['input_list'])
+                graph_drawing_cost_increase(st.session_state['input_list'], z_star, next_z_star, tuple_Arcs, tuple_A)
         else:
             if option_constraints == 'free (Interdict any number of arcs at a time)':
                 st.text('作成中')
